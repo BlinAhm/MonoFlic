641e6a053ec5bfe4f7c081457f695192572fcc44
aarbermorina <am51551@ubt-uni.net>
2023-02-09 22:06:06 +0100

    homepage edits

--- MonoFlic/ClientApp/src/components/Home.js
+++ MonoFlic/ClientApp/src/components/Home.js
@@ -1,3 +1,4 @@
+import './homepage.css';
 import React, { Component } from 'react';
 
 export class Home extends Component {
@@ -6,10 +7,127 @@ export class Home extends Component {
 
     render() {
         return (
-            <div>
-                <video width="750" height="500" controls >
-                    <source src="https://localhost:7116/Api/Stream/Get/1" type="video/mp4" />
-                </video>
+            <div class="movies">
+                <h1>Featured Movies</h1>
+
+                <div class="image">
+                    <img class="image__img" src="https://i.ytimg.com/vi/AHmCH7iB_IM/maxresdefault.jpg" alt="Creed III" />
+                    <div class="image__overlay image__overlay--primary">
+                        <div class="image__title">Creed III</div>
+                    </div>
+
+                </div>
+
+                <div class="image">
+                    <img class="image__img" src="https://i.ytimg.com/vi/qEVUtrk8_B4/maxresdefault.jpg" alt="John Wick: chapter 4" />
+                    <div class="image__overlay image__overlay--primary">
+                        <div class="image__title">John Wick: Chapter 4</div>
+                    </div>
+
+                </div>
+
+                <div class="image">
+                    <img class="image__img" src="https://i.ytimg.com/vi/uLtkt8BonwM/maxresdefault.jpg" alt="Last Of Us" />
+                    <div class="image__overlay image__overlay--primary">
+                        <div class="image__title">Last of Us</div>
+                    </div>
+
+                </div>
+
+                <div class="image">
+                    <img class="image__img" src="https://i.ytimg.com/vi/05w_GiCN39Y/maxresdefault.jpg" alt="Triple Frontier" />
+                    <div class="image__overlay image__overlay--primary">
+                        <div class="image__title">Triple Frontier</div>
+                    </div>
+
+                </div>
+                <div class="image">
+                    <img class="image__img" src="https://i.ytimg.com/vi/fEE4RO-_jug/maxresdefault.jpg" alt="FF9" />
+                    <div class="image__overlay image__overlay--primary">
+                        <div class="image__title">Fast and Furious 9</div>
+                    </div>
+
+                </div>
+                <div class="image">
+                    <img class="image__img" src="https://i.ytimg.com/vi/hOAgHDsQrQY/maxresdefault.jpg" alt="Cars 3" />
+                    <div class="image__overlay image__overlay--primary">
+                        <div class="image__title">Cars 3</div>
+                    </div>
+
+                </div>
+                <div class="image">
+                    <img class="image__img" src="https://i.ytimg.com/vi/hflCiNtY6MA/maxresdefault.jpg" alt="Oppenheimer" />
+                    <div class="image__overlay image__overlay--primary">
+                        <div class="image__title">Oppenheimer</div>
+                    </div>
+
+                </div>
+                <div class="image">
+                    <img class="image__img" src="https://i.ytimg.com/vi/eFYUX9l-m5I/maxresdefault.jpg" alt="AMCO" />
+                    <div class="image__overlay image__overlay--primary">
+                        <div class="image__title">A Man Called Otto</div>
+                    </div>
+
+                </div>
+                <h1>Featured TV Shows</h1>
+
+                <div class="image">
+                    <img class="image__img" src="https://i.ytimg.com/vi/VcQvAze0_PI/maxresdefault.jpg" />
+                    <div class="image__overlay image__overlay--primary">
+                        <div class="image__title">The Office</div>
+                    </div>
+
+                </div>
+                <div class="image">
+                    <img class="image__img" src="https://i.ytimg.com/vi/IcMxe5b_VUs/maxresdefault.jpg" />
+                    <div class="image__overlay image__overlay--primary">
+                        <div class="image__title">Friends</div>
+                    </div>
+
+                </div>
+
+                <div class="image">
+                    <img class="image__img" src="https://i.ytimg.com/vi/_HQQSUNRUHI/maxresdefault.jpg" />
+                    <div class="image__overlay image__overlay--primary">
+                        <div class="image__title">How I Met Your Mother</div>
+                    </div>
+
+                </div>
+                <div class="image">
+                    <img class="image__img" src="https://i.ytimg.com/vi/BZ5OCIJVErw/maxresdefault.jpg" />
+                    <div class="image__overlay image__overlay--primary">
+                        <div class="image__title">Dark</div>
+                    </div>
+
+                </div>
+                <div class="image">
+                    <img class="image__img" src="https://i.ytimg.com/vi/k4pSqXVhZAA/maxresdefault.jpg" />
+                    <div class="image__overlay image__overlay--primary">
+                        <div class="image__title">Designated Survivor</div>
+                    </div>
+
+                </div>
+                <div class="image">
+                    <img class="image__img" src="https://i.ytimg.com/vi/ceqOTZnhgY8/maxresdefault.jpg" />
+                    <div class="image__overlay image__overlay--primary">
+                        <div class="image__title">Breaking Bad</div>
+                    </div>
+
+                </div>
+                <div class="image">
+                    <img class="image__img" src="https://i.ytimg.com/vi/mactiVSU0m0/maxresdefault.jpg" />
+                    <div class="image__overlay image__overlay--primary">
+                        <div class="image__title">Better Call Saul</div>
+                    </div>
+
+                </div>
+                <div class="image">
+                    <img class="image__img" src="https://i.ytimg.com/vi/AL9zLctDJaU/maxresdefault.jpg" />
+                    <div class="image__overlay image__overlay--primary">
+                        <div class="image__title">Prison Break</div>
+                    </div>
+
+                </div>
             </div>
         );
     }
--- MonoFlic/ClientApp/src/components/NavMenu.js
+++ MonoFlic/ClientApp/src/components/NavMenu.js
@@ -34,17 +34,20 @@ export class NavMenu extends Component {
                 <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+                <NavLink tag={Link} className="text-dark" to="/tvshows">My List</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                <NavLink tag={Link} className="text-dark" to="/movies">Browse</NavLink>
+              </NavItem>
+              <NavItem>
+              <NavLink tag={Link} className="text-dark" to="/mylist">My List</NavLink>
               </NavItem>
               <LoginMenu>
               </LoginMenu>
             </ul>
           </Collapse>
         </Navbar>
-      </header>
+        </header>
     );
   }
 }
